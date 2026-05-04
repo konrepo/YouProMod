c7cf75d9489127c388314e361f7ad4539965839d
konrepo <37514522+konrepo@users.noreply.github.com>
2026-05-04 15:13:12 -0400

    Update clone_tweaks.sh

--- scripts/clone_tweaks.sh
+++ scripts/clone_tweaks.sh
@@ -24,8 +24,8 @@ clone_group() {
 }
 
 # Repos by owner
-clone_group Tonwalter888 YouMod
-clone_group PoomSmart YTVideoOverlay YouPiP YouMute YTUHD YouChooseQuality YouGroupSettings YouSpeed
+clone_group Tonwalter888 YouMod YTUHD
+clone_group PoomSmart YTVideoOverlay YouPiP YouMute YouChooseQuality YouGroupSettings YouSpeed
 
 # DontEatMyContent
 if [ "${INPUT_DEMC:-false}" = "true" ]; then
