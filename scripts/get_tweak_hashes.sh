82d526c94f91833ba53dd42653663a195b13554c
konrepo <37514522+konrepo@users.noreply.github.com>
2026-05-04 08:48:24 -0400

    Update get_tweak_hashes.sh

--- scripts/get_tweak_hashes.sh
+++ scripts/get_tweak_hashes.sh
@@ -8,11 +8,14 @@ get_hash() {
 echo "==> Fetching tweak hashes"
 
 youmod=$(get_hash https://github.com/Tonwalter888/YouMod.git)
+ytvideooverlay=$(get_hash https://github.com/PoomSmart/YTVideoOverlay.git)
 
 echo "youmod=$youmod" >> "$GITHUB_OUTPUT"
+echo "ytvideooverlay=$ytvideooverlay" >> "$GITHUB_OUTPUT"
 
 cat <<EOF > tweak_hashes.txt
 youmod:$youmod
+ytvideooverlay:$ytvideooverlay
 EOF
 
 echo "==> Hashes saved"
