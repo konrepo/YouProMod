0398e91485d71d9c2c65f29298f1d11e095f13db
konrepo <37514522+konrepo@users.noreply.github.com>
2026-05-04 10:32:12 -0400

    Update get_tweak_hashes.sh

--- scripts/get_tweak_hashes.sh
+++ scripts/get_tweak_hashes.sh
@@ -21,8 +21,8 @@ fetch_group() {
 echo "==> Fetching tweak hashes"
 : > tweak_hashes.txt
 
-fetch_group Tonwalter888 YouMod
-fetch_group PoomSmart YTVideoOverlay YouPiP YouMute YouChooseQuality
+fetch_group Tonwalter888 YouMod YouPiP YouMute YouChooseQuality YTUHD YouGroupSettings YouSpeed
+fetch_group PoomSmart YTVideoOverlay
 
 echo "==> Hashes saved"
 cat tweak_hashes.txt
