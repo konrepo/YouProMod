ce8533f80c33ebc95b44c4245fb7588aff5f1713
konrepo <37514522+konrepo@users.noreply.github.com>
2026-05-07 14:08:57 -0400

    Update get_tweak_hashes.sh

--- scripts/get_tweak_hashes.sh
+++ scripts/get_tweak_hashes.sh
@@ -24,7 +24,8 @@ echo "==> Fetching tweak hashes"
 : > tweak_hashes.txt
 
 # Core
-fetch_group Tonwalter888 YouMod YTUHD YouMute
+fetch_group konrepo YouMute
+fetch_group Tonwalter888 YouMod YTUHD
 fetch_group PoomSmart YTVideoOverlay YouPiP YouChooseQuality YouGroupSettings YouSpeed
 
 # DontEatMyContent
