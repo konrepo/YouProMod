9179c95614629a0820155efad5c7b948ea04c11d
konrepo <37514522+konrepo@users.noreply.github.com>
2026-05-07 14:08:25 -0400

    Update clone_tweaks.sh

--- scripts/clone_tweaks.sh
+++ scripts/clone_tweaks.sh
@@ -28,7 +28,8 @@ clone_group() {
 }
 
 # Repos by owner
-clone_group Tonwalter888 YouMod YTUHD YouMute
+clone_group konrepo YouMute
+clone_group Tonwalter888 YouMod YTUHD
 clone_group PoomSmart YTVideoOverlay YouPiP YouChooseQuality YouGroupSettings YouSpeed
 
 # DontEatMyContent
