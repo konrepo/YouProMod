b5e37c7d2de643c6f7c44f3e0935eb136cd395f3
konrepo <37514522+konrepo@users.noreply.github.com>
2026-05-08 22:34:30 -0400

    Update package_ipa.sh

--- scripts/package_ipa.sh
+++ scripts/package_ipa.sh
@@ -7,21 +7,6 @@ ROOT="${GITHUB_WORKSPACE:-$(pwd)}"
 echo "==> Preparing tweak injection"
 echo "==> INPUT_DEMC=${INPUT_DEMC:-false}"
 
-#case "$VERSION" in
-#  beta1)
-#    YOUPRO_DYLIB="$ROOT/tweaks/YouProBeta1.dylib"
-#    LANGFIX_DEB="$ROOT/youprolangfix.deb"
-#    ;;
-#  beta3)
-#    YOUPRO_DYLIB="$ROOT/tweaks/YouProBeta3.dylib"
-#    LANGFIX_DEB="$ROOT/youprob2langfix.deb"
-#    ;;
-#  *)
-#    echo "::error::Invalid YouPro version: $VERSION"
-#    exit 1
-#    ;;
-#esac
-
 inject_items=(
   "OpenYouTubeSafariExtension.appex"
   "$ROOT/youmod.deb"
@@ -40,9 +25,6 @@ fi
 
 inject_items+=(
   "$ROOT/khmertopbutton.deb"
-  #"$LANGFIX_DEB"
-  #"$YOUPRO_DYLIB"
-  #"$ROOT/tweaks/YouPro.bundle"
 )
 
 for item in "${inject_items[@]}"; do
@@ -51,9 +33,6 @@ for item in "${inject_items[@]}"; do
     exit 1
   fi
 done
-
-# this if for YouPro
-#OUTPUT_NAME="YouProMod-${VERSION}-${GITHUB_RUN_NUMBER}.ipa" 
 
 OUTPUT_NAME="YouProMod-${GITHUB_RUN_NUMBER}.ipa"
 
