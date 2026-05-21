442ef773657b9edd5a6cb9ea140f68edfecaa301
konrepo <37514522+konrepo@users.noreply.github.com>
2026-05-21 10:32:35 -0400

    Update patch_ytlocalqueue.sh

--- scripts/patch_ytlocalqueue.sh
+++ scripts/patch_ytlocalqueue.sh
@@ -13,18 +13,19 @@ if not file.is_file():
 
 text = file.read_text()
 
+# 1) Playlist / renderer menu path
 text = text.replace(
     "for (NSUInteger i = 0; i < actions.count; i++) {",
     "for (NSInteger i = 0; i < (NSInteger)actions.count; i++) {",
     1
 )
 
-old = '''if ([t containsString:@"play next in queue"]) { 
+old_menu = '''if ([t containsString:@"play next in queue"]) { 
                     queueIndex = i; 
                     break; 
                 }'''
 
-new = '''if ([t isEqualToString:@"download video"] ||
+new_menu = '''if ([t isEqualToString:@"download video"] ||
                     [t isEqualToString:@"download"]) {
                     [actions removeObjectAtIndex:i];
                     i--;
@@ -36,10 +37,48 @@ new = '''if ([t isEqualToString:@"download video"] ||
                     break; 
                 }'''
 
-if old in text and "removeObjectAtIndex:i" not in text:
-    text = text.replace(old, new, 1)
-    file.write_text(text)
-    print("Patched YTLocalQueue menu")
+if old_menu in text and "removeObjectAtIndex:i" not in text:
+    text = text.replace(old_menu, new_menu, 1)
+    print("Patched YTMenuController path")
 else:
-    print("Already patched or anchor not found")
+    print("YTMenuController path already patched or anchor not found")
+
+# 2) Feed / default sheet path
+old_sheet = '''    if (origDefaultSheetAddAction) origDefaultSheetAddAction(self, _cmd, action);'''
+
+new_sheet = '''    @try {
+        NSString *title = nil;
+
+        if ([action respondsToSelector:@selector(button)]) {
+            id btn = [action button];
+            if ([btn isKindOfClass:[UIButton class]]) {
+                title = [(UIButton *)btn currentTitle];
+            }
+        }
+
+        if (title.length == 0) {
+            title = [action valueForKey:@"_title"];
+        }
+
+        NSString *t = title.lowercaseString;
+        if ([t isEqualToString:@"download video"] ||
+            [t isEqualToString:@"download"]) {
+            return;
+        }
+    } @catch (__unused NSException *e) {}
+
+    if (origDefaultSheetAddAction) origDefaultSheetAddAction(self, _cmd, action);'''
+
+if old_sheet in text and "YTLPRemoveNativeDownload" not in text:
+    text = text.replace(
+        old_sheet,
+        "    // YTLPRemoveNativeDownload\n" + new_sheet,
+        1
+    )
+    print("Patched YTDefaultSheetController path")
+else:
+    print("YTDefaultSheetController path already patched or anchor not found")
+
+file.write_text(text)
+print("YTLocalQueue patch complete")
 PY
